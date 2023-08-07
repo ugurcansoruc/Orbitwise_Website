1bf71e267d7f351be240429493decc9bab34813c
ugurcansoruc <ugurcan.soruc@gmail.com>
2023-08-07 23:53:43 +0300

    Tum pageler eklendi ve hatalari giderildi.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,11 +1,22 @@
 'use client';
 
+import AboutSectionOne from "@/../components/About/AboutSectionOne";
+import AboutSectionTwo from "@/../components/About/AboutSectionTwo";
+import Breadcrumb from "@/../components/Common/Breadcrumb";
+
 import {useTranslations} from 'next-intl';
 
 export default function AboutPage() {
   const t = useTranslations('AboutPage');
 
   return (
-    <></>
+    <>
+      <Breadcrumb
+        pageName="About Page"
+        description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+      />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+    </>
   );
 }
