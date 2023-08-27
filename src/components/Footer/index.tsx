f77dfe5895f7a9f7f17a5c7b4c4c7d760c05bc34
ugurcansoruc <ugurcan.soruc@gmail.com>
2023-08-27 14:02:16 +0300

    Logo duzenlemesi
    - Logo kalitesi arttirildi.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -14,14 +14,14 @@ function Footer () {
               <div className="mb-12 max-w-[360px] lg:mb-16">
                 <Link href="/" className="mb-8 inline-block">
                   <Image
-                    src="/images/logo/orbitwise_logo_kesilmis_no_back.svg"
+                    src="/images/logo/orbitwise_logo_kesilmis_no_back.png"
                     alt="logo"
                     className="w-full dark:hidden"
                     width={140}
                     height={30}
                   />
                   <Image
-                    src="/images/logo/orbitwise_logo_kesilmis_no_back.svg"
+                    src="/images/logo/orbitwise_logo_kesilmis_no_back.png"
                     alt="logo"
                     className="hidden w-full dark:block"
                     width={140}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -59,14 +59,14 @@ function Header () {
                 } `}
               >
                 <Image
-                  src="/images/logo/orbitwise_logo_kesilmis_no_back.svg"
+                  src="/images/logo/orbitwise_logo_kesilmis_no_back.png"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
                 />
                 <Image
-                  src="/images/logo/orbitwise_logo_kesilmis_no_back.svg"
+                  src="/images/logo/orbitwise_logo_kesilmis_no_back.png"
                   alt="logo"
                   width={140}
                   height={30}
